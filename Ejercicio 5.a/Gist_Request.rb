5d3ce7ae23abd27f56e0d28b7c5e923ea6fa9161
joelito1234 <joel.alayon@anima.edu.uy>
2020-08-14 18:38:10 -0300

    Fix typos and add github logo ASCII

--- Ejercicio 5.a/Gist_Request.rb
+++ Ejercicio 5.a/Gist_Request.rb
@@ -22,7 +22,14 @@ class GistRequest
     uri = URI.parse(GITHUB_URL)
     request = Net::HTTP::Post.new(uri)
     request.basic_auth(ENV['YOUR_USERNAME'], ENV['YOUR_TOKEN'])
-    request.body = JSON.dump({ "description" => @description,"public" => @state,"files" => { @filename => { "content" => @content }}})
+    
+    request.body = JSON.dump({ 
+      "description" => @description,
+      "public" => @state,
+      "files" =>
+        { @filename => { "content" => @content }}
+      })
+    
     req_options = { use_ssl: uri.scheme == "https" }
     
     begin
@@ -36,10 +43,12 @@ class GistRequest
 
     if @response_status == HTTP_STATUS_CREATED
       puts "Tu Gist se creó, la url es " + parsed["url"]
+      puts "===================================================================================================="
     end
 
     rescue SocketError => se
       puts "Ocurrió un error de conexion, Quieres intentar de nuevo?"
+      puts "===================================================================================================="
       @try_again = gets.chomp.capitalize
     end
   end
